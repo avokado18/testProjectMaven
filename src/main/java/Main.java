50a46f392dcdf9295ad3e3be77e608cdf3c72e59
avokado18 <proskuryakova_v@inbox.ru>
2017-04-05 10:15:21 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Summator summator = new Summator();
-        System.out.println(summator.concate("hi ", "world"));
+        System.out.println(summator.concate("hello ", "world"));
         System.out.println(summator.sum(1,15));
     }
 }
