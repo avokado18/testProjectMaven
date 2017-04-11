e2534fbdeb9dd0e769fd9e4cb85dd0e3c2365350
avokado18 <proskuryakova_v@inbox.ru>
2017-04-11 23:36:38 +0300

    Update Summator.java

--- src/main/java/Summator.java
+++ src/main/java/Summator.java
@@ -1,9 +1,9 @@
 public class Summator {
     public String concate(String str1, String str2){
-        return str1 + str2;
+        return str1.concat(str2);
     }
 
     public int sum(int a, int b){
-        return a+ b;
+        return Math.addExact(a,b);
     }
 }
